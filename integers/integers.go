e1908ce6082496a69d3539a7527c1e57cc7acfbf
Dillon Jaap <dillonjaap@gmail.com>
2020-06-01 23:02:17 -0600

    initial commit

---
+++ integers/integers.go
@@ -0,0 +1,12 @@
+package integers
+
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
+	return x + y
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
