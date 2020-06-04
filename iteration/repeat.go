7792a42066399e29e3e2c5c099d128d57487e563
Dillon Jaap <dillonjaap@gmail.com>
2020-06-03 19:36:01 -0600

    more

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,17 @@
 package iteration
+import "fmt"
 
-func Repeat(character string) string {
+func Repeat(character string, n int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		repeated += character
 	}
 
 	return repeated
 }
 
+func ExampleRepeat() {
+	repeated := Repeat("a", 4)
+	fmt.Println(repeated)
+	// Output: aaaa
+}
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,16 +3,17 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 4)
+	expected := "aaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 4)
 	}
 }
